7d4171b11106ca4c029b6a41a18032fa78fdd3a4
Robert Scholey <robertscholey98@gmail.com>
2023-04-02 19:18:10 +0100

    small refactor at highest level index.ts

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const formatFileContents_1 = __importDefault(require("./formatFileContents"));
 const readFile_1 = __importDefault(require("./readFile"));
 const isWinner_1 = __importDefault(require("./isWinner"));
-const fileContents = (0, readFile_1.default)('files/input.txt');
-const formattedData = (0, formatFileContents_1.default)(fileContents);
-formattedData.cards.map((card, index) => console.log(`Card ${index + 1} is a ${(0, isWinner_1.default)(card, formattedData.winningNumbers) ? 'winner congratulations!' : 'not a winner booo!'}`));
+function deduceWinnersFromFile(relativePath) {
+    const fileContents = (0, readFile_1.default)(relativePath);
+    const formattedData = (0, formatFileContents_1.default)(fileContents);
+    formattedData.cards.map((card, index) => console.log(`Card ${index + 1} is a ${(0, isWinner_1.default)(card, formattedData.winningNumbers) ? 'winner congratulations!' : 'not a winner booo!'}`));
+}
+deduceWinnersFromFile('files/input.txt');
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import readFile from "./readFile";
 import isWinner from "./isWinner";
 import { StandardBingoCard } from "../model/card";
 
-const fileContents = readFile('files/input.txt')
-const formattedData = formatFileContents(fileContents);
-formattedData.cards.map((card: StandardBingoCard, index: number) => 
-    console.log(`Card ${index + 1} is a ${isWinner(card, formattedData.winningNumbers) ? 'winner congratulations!' : 'not a winner booo!'}`)
-)
+function deduceWinnersFromFile(relativePath: string) {
+    const fileContents = readFile(relativePath)
+    const formattedData = formatFileContents(fileContents);
+    formattedData.cards.map((card: StandardBingoCard, index: number) => 
+        console.log(`Card ${index + 1} is a ${isWinner(card, formattedData.winningNumbers) ? 'winner congratulations!' : 'not a winner booo!'}`)
+    )
+}
 
+deduceWinnersFromFile('files/input.txt');
