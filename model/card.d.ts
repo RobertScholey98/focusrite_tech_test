19c3cf91f1adbf881d7469c119d11ae1594d9fe7
Robert Scholey <robertscholey98@gmail.com>
2023-04-02 16:26:27 +0100

    created a barebones node project with typescript and nodemon. Added a dynamic type for BingoCard and a 5x5 type for StandardBingoCard

---
+++ model/card.d.ts
@@ -0,0 +1,11 @@
+type FixedLengthArray<N extends number, T> = [T, ...T[]] & { length: N };
+//  An array of fixed length N
+
+type BingoCard<N extends number> = FixedLengthArray<N, FixedLengthArray<N, number>>;
+//  A grid ([][]) where both the inner and outer arrays have fixed length N 
+
+export type StandardBingoCard = BingoCard<5>;
+//  Standard 5x5 bingo card
+
+export type MegaBingoCard = BingoCard<7>;
+//  If we wanted to extend the program to work with 7x7 bingo cards
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('hello focusrite');
+
+const notNumber: string = '6';
+const test: number = notNumber + 1;
